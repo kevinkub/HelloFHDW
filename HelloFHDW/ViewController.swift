53375f4b178ac334603df4fdf12f2e56b49aaea6
Kevin Kub <github@kekub.de>
2016-02-21 17:12:47 +0100

    Hello FHDW Dummy App

---
+++ HelloFHDW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloFHDW
+//
+//  Created by Kevin Kub on 21.02.16.
+//  Copyright © 2016 Kevin Kub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
